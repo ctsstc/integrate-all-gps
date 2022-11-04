cf15c0f2d9a77d8895e55f42e0a2e6297176f087
Cody Swartz <ctsstc@gmail.com>
2022-11-04 00:36:45 -0700

    Initial dry run

--- src/main.mjs
+++ src/main.mjs
@@ -2,13 +2,30 @@
 // import * as zx from 'zx'
 import 'zx/globals'
 
-const gitStatus = await $`git status -sb`.quiet()
-const matches = gitStatus.toString().match(/ahead (\d+), behind (\d+)/)
+const gitStatus = await $`git fetch && git status -sb`.quiet()
+const aheadMatches = gitStatus.toString().match(/ahead (\d+)/)
+const behindMatches = gitStatus.toString().match(/behind (\d+)/)
 
-if (matches) {
-  const [_, ahead, behind] = matches
-  console.dir({ ahead, behind }, { depth: null })
-}
-else {
-  console.log('no match')
+const [_, aheadStr] = aheadMatches || [null, '0']
+const [__, behindStr] = behindMatches || [null, '0']
+const ahead = parseInt(aheadStr)
+const behind = parseInt(behindStr)
+
+console.dir({ ahead, behind }, { depth: null })
+
+if (ahead > 0) {
+
+  if (behind > 0) {
+    console.log('Behind, pulling...')
+    // await $`gps pull`
+  }
+
+  console.log('Integrating...')
+  // await $`gps int 0 -f`
+
+  console.log('Pulling...')
+  // await $`gps pull`
+
+  console.log('Current Patches...')
+  // await $`gps ls`
 }
